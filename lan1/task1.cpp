f0073c1719c54a6a2de38c2f90662cf1fa01aeea
syedfahadshah-f7 <142867460+syedfahadshah-f7@users.noreply.github.com>
2024-01-25 14:45:45 +0500

    Create task1.cpp

---
+++ lan1/task1.cpp
@@ -0,0 +1,7 @@
+for(int i=0;i<num_std;i++){
+        cout<<"Enter Student"<<i+1<<"course"<<endl;
+        for(int j=0;j<3;j++){
+            cout<<"Enter Course"<<j+1<<endl;
+            cin>>arr[i][j];
+        }
+    }
