d22c21ccfaf3e769381b424ed200e01aa5da351a
syedfahadshah-f7 <142867460+syedfahadshah-f7@users.noreply.github.com>
2024-01-28 13:40:46 +0500

    Create task4.cpp

---
+++ lan1/task4.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int max_area(int *height, int n) {
+    int max_area = 0;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (i == j) continue;
+
+            max_area = max(max_area, abs(i - j) *min(height[i], height[j]));
+        }
+    }
+
+    return max_area;
+}
+
+int main() {
+
+    int heights[9] = {1, 8, 6, 2, 5, 4, 8, 3, 7};
+
+    cout << max_area(heights, 9) <<endl;
+
+    return 0;
+}
