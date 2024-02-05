730c5a61930a7fe091f2bd5a629fb14e994556a5
syedfahadshah-f7 <142867460+syedfahadshah-f7@users.noreply.github.com>
2024-02-05 15:30:28 +0500

    Create task 6.cpp

---
+++ lab 02/task 6.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int LCM (int a,int b){
+    static int maxi=max(a,b), mini=min(a,b), res=maxi;
+    if (res % mini == 0) return res;
+    res += maxi;
+    return LCM(mini,maxi);
+}
+int GCD(int a, int b){
+    static int mini = min(a,b), maxi=max(a,b);
+    if(maxi % mini == 0) return mini;
+    return GCD(mini,maxi);
+}
+int main (){
+    cout<<"Enter Two Numbers "<<endl;
+    int a,b;
+    cin>>a>>b;
+    cout<<"LCM :"<<LCM(a,b)<<endl;
+    cout<<"GCD :"<<GCD(a,b)<<endl;
+}
