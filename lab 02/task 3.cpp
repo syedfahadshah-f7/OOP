1d3dade47ab28364eaf4f8927ef65b7aee596b21
syedfahadshah-f7 <142867460+syedfahadshah-f7@users.noreply.github.com>
2024-02-05 15:28:29 +0500

    Create task 3.cpp

---
+++ lab 02/task 3.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+bool hasSubsetSum(int arr[], int size, int targetSum){
+    static int i=0;
+    if(i == size) return false;
+    int sum =arr[i];
+    for(int j=i+1;sum <= targetSum;j++){
+        sum += arr[j];
+        if(sum == targetSum) return true;
+    }
+    i++;
+    return hasSubsetSum(arr,size,targetSum);
+}
+int main(){
+    int size, target_sum;
+    cout<<"Enter Size Of Array"<<endl;
+    cin>>size;
+    int arr[5]={6,9,2,5,7};
+    cout<<"Enter Target Sum"<<endl;
+    cin>>target_sum;
+
+    if(hasSubsetSum(arr,size,target_sum) == 1){
+        cout<<"Subset is present ";
+        return 0;
+    }else{
+        cout<<"Subset is not present ";
+        return 0;
+    }
+}
